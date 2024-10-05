b7c767a3ed8a708be5b78664009d5a7bd3833c50
Sithanga Rashmika Sandaruwan <87372439+Sithanga-Rashmika@users.noreply.github.com>
2024-10-05 20:25:17 +0530

    dashboard complete

--- Web/src/componants/navbar.jsx
+++ Web/src/componants/navbar.jsx
@@ -3,66 +3,102 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <header class="app-header">
-        <nav class="navbar navbar-expand-lg navbar-light">
-          <ul class="navbar-nav">
-            <li class="nav-item d-block d-xl-none">
+      <header className="app-header">
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <ul className="navbar-nav">
+            <li className="nav-item d-block d-xl-none">
               <a
-                class="nav-link sidebartoggler nav-icon-hover"
+                className="nav-link sidebartoggler nav-icon-hover"
                 id="headerCollapse"
                 href="javascript:void(0)"
               >
-                <i class="ti ti-menu-2"></i>
+                <i className="ti ti-menu-2"></i>
               </a>
             </li>
           </ul>
-          <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
-            <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
-              <li class="nav-item dropdown">
+          <div className="navbar-collapse justify-content-end px-0" id="navbarNav">
+            <ul className="navbar-nav flex-row ms-auto align-items-center justify-content-end">
+              <li className="nav-item dropdown">
                 <a
-                  class="nav-link nav-icon-hover"
+                  className="nav-link nav-icon-hover"
+                  href="javascript:void(0)"
+                  id="notificationDropdown"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <i className="ti ti-bell-ringing"></i>
+                  <div className="notification bg-primary rounded-circle"></div>
+                </a>
+                <div
+                  className="dropdown-menu dropdown-menu-end dropdown-menu-animate-up"
+                  aria-labelledby="notificationDropdown"
+                  style={{ width: "20rem", zIndex:"10" }}
+                >
+                  <div className="notification-body p-3">
+                    <h4 style={{ fontSize: "1.25rem", color: "#007bff" }}>
+                      Notifications
+                    </h4>
+                    <a href="javascript:void(0)" className="dropdown-item">
+                      New comment on your post
+                    </a>
+                    <a href="javascript:void(0)" className="dropdown-item">
+                      You have 5 new messages
+                    </a>
+                    <a href="javascript:void(0)" className="dropdown-item">
+                      Your profile has been updated
+                    </a>
+                    <a href="javascript:void(0)" className="dropdown-item">
+                      New friend request
+                    </a>
+                  </div>
+                </div>
+              </li>
+
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link nav-icon-hover"
                   href="javascript:void(0)"
                   id="drop2"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
                 >
                   <img
-                    src="../assets/images/profile/user-1.jpg"
+                    src="../assets/images/profile/user.jpeg"
                     alt=""
                     width="35"
                     height="35"
-                    class="rounded-circle"
+                    className="rounded-circle"
                   />
                 </a>
                 <div
-                  class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up"
+                  className="dropdown-menu dropdown-menu-end dropdown-menu-animate-up"
                   aria-labelledby="drop2"
                 >
-                  <div class="message-body">
+                  <div className="message-body">
                     <a
                       href="javascript:void(0)"
-                      class="d-flex align-items-center gap-2 dropdown-item"
+                      className="d-flex align-items-center gap-2 dropdown-item"
                     >
-                      <i class="ti ti-user fs-6"></i>
-                      <p class="mb-0 fs-3">My Profile</p>
+                      <i className="ti ti-user fs-6"></i>
+                      <p className="mb-0 fs-3">My Profile</p>
                     </a>
                     <a
                       href="javascript:void(0)"
-                      class="d-flex align-items-center gap-2 dropdown-item"
+                      className="d-flex align-items-center gap-2 dropdown-item"
                     >
-                      <i class="ti ti-mail fs-6"></i>
-                      <p class="mb-0 fs-3">My Account</p>
+                      <i className="ti ti-mail fs-6"></i>
+                      <p className="mb-0 fs-3">My Account</p>
                     </a>
                     <a
                       href="javascript:void(0)"
-                      class="d-flex align-items-center gap-2 dropdown-item"
+                      className="d-flex align-items-center gap-2 dropdown-item"
                     >
-                      <i class="ti ti-list-check fs-6"></i>
-                      <p class="mb-0 fs-3">My Task</p>
+                      <i className="ti ti-list-check fs-6"></i>
+                      <p className="mb-0 fs-3">My Task</p>
                     </a>
                     <a
                       href="./authentication-login.html"
-                      class="btn btn-outline-primary mx-3 mt-2 d-block"
+                      className="btn btn-outline-primary mx-3 mt-2 d-block"
                     >
                       Logout
                     </a>
--- Web/src/componants/sidebar.jsx
+++ Web/src/componants/sidebar.jsx
@@ -7,11 +7,7 @@ const Sidebar = () => {
         <div>
           <div class="brand-logo d-flex align-items-center justify-content-between">
             <a href="./index.html" class="text-nowrap logo-img">
-              <img
-                src="../assets/images/logos/dark-logo.svg"
-                width="180"
-                alt=""
-              />
+              <img src="../assets/images/logos/logo1.png" width="180" alt="" />
             </a>
             <div
               class="close-btn d-xl-none d-block sidebartoggler cursor-pointer"
@@ -40,7 +36,7 @@ const Sidebar = () => {
               </li>
               <li class="nav-small-cap">
                 <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-                <span class="hide-menu">UI COMPONENTS</span>
+                <span class="hide-menu">Product Management</span>
               </li>
               <li class="sidebar-item">
                 <a
@@ -49,9 +45,9 @@ const Sidebar = () => {
                   aria-expanded="false"
                 >
                   <span>
-                    <i class="ti ti-article"></i>
+                    <i class="ti ti-package"></i>
                   </span>
-                  <span class="hide-menu">Buttons</span>
+                  <span class="hide-menu">Manage Product</span>
                 </a>
               </li>
               <li class="sidebar-item">
@@ -61,11 +57,17 @@ const Sidebar = () => {
                   aria-expanded="false"
                 >
                   <span>
-                    <i class="ti ti-alert-circle"></i>
+                    <i class="ti ti-layers"></i>
                   </span>
-                  <span class="hide-menu">Alerts</span>
+                  <span class="hide-menu">Manage Categories</span>
                 </a>
               </li>
+
+              <li class="nav-small-cap">
+                <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                <span class="hide-menu">Inventory Management</span>
+              </li>
+
               <li class="sidebar-item">
                 <a
                   class="sidebar-link"
@@ -73,11 +75,17 @@ const Sidebar = () => {
                   aria-expanded="false"
                 >
                   <span>
-                    <i class="ti ti-cards"></i>
+                    <i class="ti ti-box"></i>
                   </span>
-                  <span class="hide-menu">Card</span>
+                  <span class="hide-menu">Inventory Control</span>
                 </a>
               </li>
+
+              <li class="nav-small-cap">
+                <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                <span class="hide-menu">Order Management</span>
+              </li>
+
               <li class="sidebar-item">
                 <a
                   class="sidebar-link"
@@ -85,9 +93,9 @@ const Sidebar = () => {
                   aria-expanded="false"
                 >
                   <span>
-                    <i class="ti ti-file-description"></i>
+                    <i class="ti ti-receipt"></i>
                   </span>
-                  <span class="hide-menu">Forms</span>
+                  <span class="hide-menu">Orders List</span>
                 </a>
               </li>
               <li class="sidebar-item">
@@ -97,14 +105,15 @@ const Sidebar = () => {
                   aria-expanded="false"
                 >
                   <span>
-                    <i class="ti ti-typography"></i>
+                    <i class="ti ti-trash"></i>
                   </span>
-                  <span class="hide-menu">Typography</span>
+                  <span class="hide-menu">Cancellations Request</span>
                 </a>
               </li>
+
               <li class="nav-small-cap">
                 <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-                <span class="hide-menu">AUTH</span>
+                <span class="hide-menu">Vendor Management</span>
               </li>
               <li class="sidebar-item">
                 <a
@@ -113,9 +122,27 @@ const Sidebar = () => {
                   aria-expanded="false"
                 >
                   <span>
-                    <i class="ti ti-login"></i>
+                    <i class="ti ti-briefcase"></i>
                   </span>
-                  <span class="hide-menu">Login</span>
+                  <span class="hide-menu">Vendor Registration</span>
+                </a>
+              </li>
+
+              <li class="nav-small-cap">
+                <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
+                <span class="hide-menu">User Management</span>
+              </li>
+
+              <li class="sidebar-item">
+                <a
+                  class="sidebar-link"
+                  href="./authentication-register.html"
+                  aria-expanded="false"
+                >
+                  <span>
+                    <i class="ti ti-user-check"></i>
+                  </span>
+                  <span class="hide-menu">Account Activation</span>
                 </a>
               </li>
               <li class="sidebar-item">
@@ -127,35 +154,7 @@ const Sidebar = () => {
                   <span>
                     <i class="ti ti-user-plus"></i>
                   </span>
-                  <span class="hide-menu">Register</span>
-                </a>
-              </li>
-              <li class="nav-small-cap">
-                <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-                <span class="hide-menu">EXTRA</span>
-              </li>
-              <li class="sidebar-item">
-                <a
-                  class="sidebar-link"
-                  href="./icon-tabler.html"
-                  aria-expanded="false"
-                >
-                  <span>
-                    <i class="ti ti-mood-happy"></i>
-                  </span>
-                  <span class="hide-menu">Icons</span>
-                </a>
-              </li>
-              <li class="sidebar-item">
-                <a
-                  class="sidebar-link"
-                  href="./sample-page.html"
-                  aria-expanded="false"
-                >
-                  <span>
-                    <i class="ti ti-aperture"></i>
-                  </span>
-                  <span class="hide-menu">Sample Page</span>
+                  <span class="hide-menu">Account Reactivation</span>
                 </a>
               </li>
             </ul>
